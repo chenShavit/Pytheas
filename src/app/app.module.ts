131d8c9a6200c4aeeefb2ce2c63789711d410b56
cShavit68 <ck20427@gmail.com>
2019-07-23 10:33:38 +0300

    Add google 0Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,14 +39,14 @@ import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
 import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
 
 let config = new AuthServiceConfig([
-  {
-    id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
-  },
-  {
-    id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider("Facebook-App-Id")
-  }
+  // {
+  //   id: GoogleLoginProvider.PROVIDER_ID,
+  //   provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
+  // },
+  // {
+  //   id: FacebookLoginProvider.PROVIDER_ID,
+  //   provider: new FacebookLoginProvider("Facebook-App-Id")
+  // }
 ]);
 export function provideConfig() {
   return config;
